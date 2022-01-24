375bfe94f574243cc2de04f0aadbedf4f7b5b7b3
AdaamD <98329603+AdaamD@users.noreply.github.com>
2022-01-24 16:29:46 +0100

    Add files via upload

---
+++ Personnage.java
@@ -0,0 +1,26 @@
+
+public class Personnage {
+
+	private String Type_de_sport;
+	private String Type_Age;  //age
+	private String Nationalité;
+	private String Type_équipement;
+	private String Sexe;
+	private String Couleur_Cheveux;
+	private String Pilosité;
+
+	//Constructeurs
+
+	public Personnage( String Type_de_sport, String Type_Age, String Nationalité, String Type_équipement, String Sexe, String Couleur_Cheveux, String Pilosité ){
+		this.Type_de_sport=Type_de_sport;
+		this.Type_Age=Type_Age;
+		this.Nationalité=Nationalité;
+		this.Type_équipement=Type_équipement;
+		this.Sexe=Sexe;
+		this.Couleur_Cheveux=Couleur_Cheveux;
+		this.Pilosité=Pilosité ;
+	}
+
+
+
+}
