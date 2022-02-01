2cd8d6cfeb4c63885da94b951bc926e177ff8993
pauloud <paulfontaine2010@hotmail.fr>
2022-02-01 11:40:32 +0100

    correction casse dans les énumérations et remplacement de TypeAge par Age

---
+++ Dossier/Projet/Condition.java
@@ -0,0 +1,26 @@
+class Condtion {
+    private boolean absurde; 
+    private Genre genre;
+    private Nationnalite nationnalite;
+    private Sport sport;
+    private Genre genre; 
+    private Pilosite pilosite; 
+    private CouleurCheuveux couleurCheveux;
+    private Cheveux cheveux;
+    private Age age; 
+
+    private boolean egauxOuIndefinis(Object o1, Object o2){
+        return o1==NULL | o2==NULL | o1==o2; 
+        
+    }
+    public Condition(){}
+    public Condition(genre Genre){
+        this.genre = genre; 
+    }
+    public condition (Nationnalite nationnalite) {
+        this.nationnalite = nationnalite; 
+
+    }
+
+
+}
