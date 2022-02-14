5d3825c63e6cd5a184c7353917d29481782f0b4f
pauloud <paulfontaine2010@hotmail.fr>
2022-02-14 16:24:24 +0100

    la classe jeu ne compile pas encore

--- Dossier/Projet/Jeu.java
+++ Dossier/Projet/Jeu.java
@@ -1,3 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.google.gson.Gson;
+
 public class Jeu {
+    private static final Personnage personnageChoisi; 
+    private static final Personnage [] personnages ; 
+    public static boolean poserQuestion(Question question){
+        return question.surPersonnage(personnageChoisi);
+    }
+    private static Personnage[] parserPersonnages() throws IOException{
+        Path nomFichier = Path.of("personnages.json");
+        String persosJson = Files.readString(nomFichier);
+        return (new Gson().fromJson(persosJson,Personnage[].class));  
+
+
+
+    }
+    
     
 }
