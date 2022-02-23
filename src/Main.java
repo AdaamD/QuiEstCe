9baa9b46f81c3a1a5f71bfcd5f4d72c1e1d6fdc4
pauloud <57322619+pauloud@users.noreply.github.com>
2022-02-23 13:05:18 +0100

    mise à jour Main (Adam et Michel)

--- src/Main.java
+++ src/Main.java
@@ -21,31 +21,27 @@ public class Main
 
         //création des perso
 
-		Personnage  Biles_Simone  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Djokovic_Novak  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.SERBE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Florsch_Sofia  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.ALLEMANDE ,  CouleurCheveux.CLAIR ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Hamilton_Lewis  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.ANGLAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.LONG ,  Pilosite.BARBE  ) ; 
-		Personnage  Karabatic_Nikola  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Laffont_Perrine  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.FRANCAISE ,  CouleurCheveux.CLAIR ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Lebron_James  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Manaudou_Laure  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux . LONG ,  Pilosite . IMBERBE ) ; 
-		Personnage  Manaudou_Florent  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Mbappe_Kylian  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Mcgregor_Conor  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.IRLANDAISE ,  CouleurCheveux.CLAIR ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Messi_Lionel  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.ARGENTINE ,  CouleurCheveux.FONCE ,  Genre . MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Morgan_Alex  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Raikkonen_Kimi  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.PLUS_DE_QUARANTE ,  Nationalite.FINLANDAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Renard_Wendie  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Williams_Serena  =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ; 
-		Personnage  Yoka_Tony =  new  Personnage  (  Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ; 
-		Personnage  Zidane_Zinedine  =  new  Personnage  (  Sport.COLLECTIF  ,  Age.PLUS_DE_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ; 
+		Personnage  Djokovic_Novak  =  new  Personnage  (  "Novak Djokovic",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.SERBE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Hamilton_Lewis  =  new  Personnage  (  "Lewis Hamilton",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.ANGLAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.LONG ,  Pilosite.BARBE  ) ;
+		Personnage  Karabatic_Nikola  =  new  Personnage  (  "Nikola Karabatic",Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Laffont_Perrine  =  new  Personnage  (  "Perrine Laffont ",Sport.INDIVIDUEL  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.FRANCAISE ,  CouleurCheveux.CLAIR ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ;
+		Personnage  Lebron_James  =  new  Personnage  (  "LeBron James",Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Manaudou_Laure  =  new  Personnage  (  "Laure Manaudou",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux . LONG ,  Pilosite . IMBERBE ) ;
+		Personnage  Manaudou_Florent  =  new  Personnage  (  "Laurent Manaudou",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ;
+		Personnage  Mbappe_Kylian  =  new  Personnage  (  "Kylian Mbappe",Sport.COLLECTIF  ,  Age.MOINS_VINGT_CINQ ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ;
+		Personnage  Mcgregor_Conor  =  new  Personnage  (  "Conor Mcgregor",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.IRLANDAISE ,  CouleurCheveux.CLAIR ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Messi_Lionel  =  new  Personnage  (  "Lionel Messi",Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.ARGENTINE ,  CouleurCheveux.FONCE ,  Genre . MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Morgan_Alex  =  new  Personnage  (  "Alex Morgan",Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ;
+		Personnage  Raikkonen_Kimi  =  new  Personnage  (  "Kimi Raikkonen",Sport.INDIVIDUEL  ,  Age.PLUS_DE_QUARANTE ,  Nationalite.FINLANDAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ;
+		Personnage  Renard_Wendie  =  new  Personnage  (  "Wendie Renard",Sport.COLLECTIF  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ;
+		Personnage  Williams_Serena  =  new  Personnage  (  "Serena Williams",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.USA ,  CouleurCheveux.FONCE ,  Genre.FEMININ ,  Cheveux.LONG ,  Pilosite.IMBERBE  ) ;
+		Personnage  Yoka_Tony =  new  Personnage  (  "Tony Yoka",Sport.INDIVIDUEL  ,  Age.ENTRE_VINGT_CINQ_ET_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.BARBE  ) ;
+		Personnage  Zidane_Zinedine  =  new  Personnage  (  "Zinedine Zidane",Sport.COLLECTIF  ,  Age.PLUS_DE_QUARANTE ,  Nationalite.FRANCAISE ,  CouleurCheveux.FONCE ,  Genre.MASCULIN ,  Cheveux.COURT ,  Pilosite.IMBERBE  ) ;
 
 
 		ArrayList<Personnage> liste_perso = new ArrayList<Personnage> ();
 		liste_perso.add(Zidane_Zinedine);
-		liste_perso.add(Biles_Simone);
 		liste_perso.add(Djokovic_Novak);
-		liste_perso.add(Florsch_Sofia);
 		liste_perso.add(Hamilton_Lewis);
 		liste_perso.add(Karabatic_Nikola);      
 		liste_perso.add(Laffont_Perrine);
@@ -68,8 +64,7 @@ public class Main
 // PREND UN PERSO AU HASARD
 		int i= Choixalea();
 		Personnage perso = liste_perso.get(i);
-		System.out.println(liste_perso.get(i));
-
+		
 
 // Définition le nom de l'interface graphique 
 		JFrame frame = new JFrame("Qui est ce ??");
@@ -124,6 +119,8 @@ public class Main
 		ImageIcon yoka= new ImageIcon(new ImageIcon("img/yoka.jpg").getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT));
 		ImageIcon zidane= new ImageIcon(new ImageIcon("img/zidane.jpg").getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT));
 
+		ImageIcon croix= new ImageIcon(new ImageIcon("img/croix.jpg").getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT));
+
 
 //Creation des BOUTONS de personnages  
 		JButton djokovic1 = new JButton(djokovic);
@@ -144,7 +141,7 @@ public class Main
 		JButton zidane1 = new JButton(zidane);
 
 
-
+djokovic1.setBounds(40,80,200,250);
 
 // Ajout des boutons au frame
 		panel1.add(djokovic1); 
@@ -176,6 +173,9 @@ public class Main
 
  //Creation de liste des déroulantes 
 		JPanel question = new JPanel();
+		JPanel reponse =  new JPanel();
+		JPanel validation = new JPanel();
+
 	//Sport 		
 		Sport[] sport= { Sport.INDIVIDUEL , Sport.COLLECTIF};
 	//AGE	
@@ -193,6 +193,11 @@ public class Main
 		Pilosite[]pilosite={Pilosite.IMBERBE , Pilosite.BARBE};
 	//CHEVEUX 
 		Cheveux [] cheveux= {Cheveux.COURT, Cheveux.LONG , Cheveux.CHAUVE};
+	//Nom 
+		String[] nom= {"Novak Djokovic","Lewis Hamilton", "Nikola Karabatic", "Perrine Laffont",
+		"LeBron James", "Laure Manaudou", "Laurent Manaudou", "Kylian Mbappe",
+		"Conor Mcgregor","Lionel Messi","Alex Morgan","Kimi Raikkonen",
+		"Wendie Renard", "Serena Williams","Tony Yoka", "Zinedine Zidane"};
 
 
 		JComboBox ld1 = new JComboBox(nationalite);
@@ -202,6 +207,7 @@ public class Main
 		JComboBox ld5 = new JComboBox(couleurcheveux);
 		JComboBox ld6 = new JComboBox(pilosite);
 		JComboBox ld7 = new JComboBox(cheveux);
+		JComboBox ld8 = new JComboBox(nom);
 
 
 
@@ -209,32 +215,138 @@ public class Main
 
 
 		JLabel label1 = new JLabel("Bien joué c'est correct ", JLabel.CENTER);
-		JLabel label2 = new JLabel("Pas du tout, c'est faut retente !! ", JLabel.CENTER);
+		JLabel label2 = new JLabel("Pas du tout, c'est faux retente !! ", JLabel.CENTER);
+		JLabel label3 = new JLabel("Choisissez votre question ", JLabel.CENTER);
 
-JPanel reponse =new JPanel ();
+		reponse.add(label3);
+
+//Modification perso en croix
+
+		djokovic1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				djokovic1.setIcon(croix);				
+			} 
+		}); 
+		hamilton1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				hamilton1.setIcon(croix);				
+			} 
+		});
+		karabatic1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				karabatic1.setIcon(croix);				
+			} 
+		});
+		laffont1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				laffont1.setIcon(croix);				
+			} 
+		});
+		lebron1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				lebron1.setIcon(croix);				
+			} 
+		});
+		lmanaudou1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				lmanaudou1.setIcon(croix);				
+			} 
+		});
+		manaudou1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				manaudou1.setIcon(croix);				
+			} 
+		});
+		mbappe1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				mbappe1.setIcon(croix);				
+			} 
+		});
+		mcgregor1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				mcgregor1.setIcon(croix);				
+			} 
+		});
+		messi1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				messi1.setIcon(croix);				
+			} 
+		});
+		morgan1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				morgan1.setIcon(croix);				
+			} 
+		});
+		raikonen1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				raikonen1.setIcon(croix);				
+			} 
+		});
+		renard1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				renard1.setIcon(croix);				
+			} 
+		});
+		williams1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				williams1.setIcon(croix);				
+			} 
+		});
+		yoka1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				yoka1.setIcon(croix);				
+			} 
+		});
+		zidane1.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				zidane1.setIcon(croix);				
+			} 
+		});
+
 
 
 //Fonction recherche nationalite
 		ld1.addActionListener(new ActionListener() {     
 		//	@Override
 			public void actionPerformed(ActionEvent e) {
-
+				
 				if(ld1.getSelectedItem()== perso.getNationalite())
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
+
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
-				} 
-			});
+					frame.repaint();
+				}
+			} 
+		});
 
 //fonction recherche Sport
 		ld2.addActionListener(new ActionListener() {     
@@ -245,18 +357,22 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
-				} 
-			});
+					frame.repaint();
+				}
+			} 
+		});
 
 //fonction recherche Age
 		ld3.addActionListener(new ActionListener() {     
@@ -267,15 +383,19 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);					
 					label2.setVisible(true);
 					label1.setVisible(false);
+					frame.repaint();
 					;}
 				} 
 			});
@@ -288,18 +408,22 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
-				} 
-			});
+					frame.repaint();
+				}
+			} 
+		});
 
 //fonction recherche Couleur cheveux 
 		ld5.addActionListener(new ActionListener() {     
@@ -310,18 +434,22 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
-				} 
-			});
+					frame.repaint();
+				}
+			} 
+		});
 
 //fonction recherche Pilosite
 		ld6.addActionListener(new ActionListener() {     
@@ -332,18 +460,22 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
-				} 
-			});
+					frame.repaint();
+				}
+			} 
+		});
 
 //fonction recherche Cheveux
 		ld7.addActionListener(new ActionListener() {     
@@ -354,22 +486,57 @@ JPanel reponse =new JPanel ();
 				{   
 					System.out.println("ok");
 					reponse.add(label1);
+					label3.setVisible(false);
 					label1.setVisible(true);
 					label2.setVisible(false);
+					frame.repaint();
 					
 				} 
 				else {
 					System.out.println("Nooon");
 					reponse.add(label2);
+					label3.setVisible(false);
 					label2.setVisible(true);
 					label1.setVisible(false);
-					}
+					frame.repaint();
+				}
+
+
+			} 
+		});
+//Fonction recherche nom 
+		ld8.addActionListener(new ActionListener() {     
+		//	@Override
+			public void actionPerformed(ActionEvent e) {
+
+				if(ld8.getSelectedItem()== perso.getNom())
+				{   
+					System.out.println("ok");
+					label3.setVisible(false);
+					label1.setVisible(true);
+					label2.setVisible(false);
+					frame.repaint();
+					JOptionPane.showMessageDialog(null , "Félicitation vous avez trouvé le personnage !!");
+					
 				} 
-			});
+				else {
+					System.out.println("Nooon");
+					label3.setVisible(false);
+					label2.setVisible(true);
+					label1.setVisible(false);
+					frame.repaint();
+					JOptionPane.showMessageDialog(null , "Dommage, veuillez recommencer ");
+				}
+
+
+			} 
+		});
+
 
 		
 		frame.add(question);
 		frame.add(reponse);
+		frame.add(validation);
 
 		question.add(ld1);
 		question.add(ld2);
@@ -378,12 +545,15 @@ JPanel reponse =new JPanel ();
 		question.add(ld5);
 		question.add(ld6);
 		question.add(ld7);
+		validation.add(ld8);
+
 
 
 
 		frame.pack();
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.lightGray);
 		frame.setVisible(true);
 	}
 }
