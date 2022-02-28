33f1e3a8b0e94ca42d9219115adf33b7d4231761
besteeven <98329684+besteeven@users.noreply.github.com>
2022-02-28 15:14:40 +0100

    Retour à un code qui marche pour le rendu intermédiaire
    Add files via upload
    
    Update Main.java
    
    Update Main.java
    
    Update Main.java
    
    Update Main.java
    
    Update Main.java
    
    Update Main.java
    
    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import java.awt.event.*;
 import java.util.Random;
 import java.util.function.Function;
 
-
 public class Main 
 
 {
 		private static final JFrame frame = new JFrame("Qui est ce ??");
+		private static final JFrame accueil = new JFrame("Accueil");
 		private static final JLabel reponseOui = new JLabel("Oui ", JLabel.CENTER);
 		private static final JLabel reponseNon = new JLabel("Non ", JLabel.CENTER);
 		private static final JLabel choisissez = new JLabel("Choisissez votre question ", JLabel.CENTER);
@@ -48,9 +48,46 @@ public class Main
 		return nb_Alea;
 	}
 
-
 	public static void main(String[] args) 
-	{ 	JPanel question = new JPanel();
+	{ 	
+	// menus 
+		JMenuBar menu = new JMenuBar();						
+		JMenu file = new JMenu("Fichier");					
+		JMenu theme = new JMenu("Theme");					
+		JMenu help = new JMenu("Aide");	
+		JMenuItem newf = new JMenuItem("Nouveau");
+		JMenuItem load = new JMenuItem("Charger");
+		JMenuItem quit = new JMenuItem("Quitter");
+		JMenuItem aide = new JMenuItem("Aide");
+		JMenuItem credit = new JMenuItem("Credits");
+		JMenuItem coldefaut= new JMenuItem("Par defaut");
+		JMenuItem jaune= new JMenuItem("Jaune");
+		JMenuItem rose= new JMenuItem("Rose");
+		JMenuItem orange= new JMenuItem("Orange");
+		JMenuItem vert= new JMenuItem("Vert");
+		JMenuItem bleu= new JMenuItem("Bleu");
+
+		help.add(aide);
+		help.add(credit);
+		frame.add(menu);
+		file.add(newf);
+		file.add(load);
+		file.add(quit);
+		theme.add(coldefaut);
+		theme.add(jaune);
+		theme.add(orange);
+		theme.add(rose);
+		theme.add(vert);
+		theme.add(bleu);
+		quit.addActionListener(e -> { frame.setVisible(false); accueil.setVisible(true);});
+		aide.addActionListener(e -> {JOptionPane.showMessageDialog(null , "BIENVENUE sur 'QUI-EST-CE'.\n Afin de deviner le sportif aléatoirement sélectionné, vous pouvez serez aidé de:\n-sa nationalité\n -son type de sport(individuel ou collectif) \n -sa catégorie d'âge \n -son genre(masculin ou féminin)\n -et sa pilosité.","menu d'aide",JOptionPane.INFORMATION_MESSAGE);});
+		credit.addActionListener(e-> {JOptionPane.showMessageDialog(null," This game has been developped by: \n -Paul FONTAINE \n -Adam DAIA \n -Matthias BLANC \n -Michel BE  ","Credit",JOptionPane.INFORMATION_MESSAGE);});
+
+		menu.add(file);
+		menu.add(theme);
+		menu.add(help);
+
+		JPanel question = new JPanel();
 		JPanel reponse =  new JPanel();
 		reponse.add(reponseOui);
 		reponseOui.setVisible(false);
@@ -60,32 +97,13 @@ public class Main
 		JPanel validation = new JPanel();
 		Personnage[] personnages = Jeu.getPersonnages();
 		JLabel label = new JLabel("BIENVENUE SUR QUI-EST-CE?", JLabel.CENTER);
+		//frame.add(label);
 		Personnage persoChoisi = personnages[Choixalea(personnages.length)];
 		JPanel[] panelsPersos = {new JPanel(), new JPanel(), new JPanel(), new JPanel()};
 		for (int i = 0 ; i< panelsPersos.length; i++) {
 			frame.add(panelsPersos[i]); 
 		}
 	
-		JMenuBar menu = new JMenuBar();						
-		JMenu file = new JMenu("Fichier");					
-		JMenu edit = new JMenu("Edition");					
-		JMenu help = new JMenu("Aide");	
-		JMenuItem newf = new JMenuItem("Nouveau");
-		//JMenuItem open = new JMenuItem("Ouvrir");
-		JMenuItem save = new JMenuItem("Enregistrer");
-		JMenuItem quit = new JMenuItem("Quitter");
-
-		frame.add(menu);
-		file.add(newf);
-		file.add(save);
-		file.add(quit);
-		quit.addActionListener(e -> { frame.dispose();});
-
-		menu.add(file);
-		menu.add(edit);
-		menu.add(help);
-
-
 //Creation des ICON de personnages 
 ImageIcon croix= new ImageIcon(new ImageIcon(Jeu.getImage("croix.jpg")).getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT));
 		for (int i = 0; i<personnages.length;i++){
@@ -101,12 +119,9 @@ ImageIcon croix= new ImageIcon(new ImageIcon(Jeu.getImage("croix.jpg")).getImage
 					bouton.setIcon(croix);				
 				} 
 			}); 
-			
-
 		}
-
-		frame.add(label);
-		frame.setLayout(new GridLayout(9,1));
+	
+		frame.setLayout(new GridLayout(8,1));
 		
 		JComboBox<Nationalite> nationalites = new JComboBox<> (Nationalite.values());
 		assignerQuestion(nationalites, n -> n == persoChoisi.getNationalite());
@@ -150,6 +165,87 @@ ImageIcon croix= new ImageIcon(new ImageIcon(Jeu.getImage("croix.jpg")).getImage
 
 			} 
 		});
+
+		// changer l'icone de l'interface
+		Image icone = Toolkit.getDefaultToolkit().getImage(Jeu.getImage("anneaux.png"));
+		frame.setIconImage(icone);
+		accueil.setIconImage(icone);
+
+		//définition des différents thèmes
+		coldefaut.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.WHITE);
+			menu.setBackground(Color.WHITE);
+			question.setBackground(Color.WHITE);reponse.setBackground(Color.WHITE);validation.setBackground(Color.WHITE);
+			frame.getContentPane().setBackground(Color.lightGray);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.WHITE));
+			accueil.getContentPane().setBackground(Color.lightGray);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.WHITE));
+		});
+		jaune.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.YELLOW);
+			menu.setBackground(Color.YELLOW);
+			question.setBackground(Color.YELLOW);reponse.setBackground(Color.YELLOW);validation.setBackground(Color.YELLOW);
+			frame.setBackground(Color.YELLOW);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.YELLOW));
+			accueil.getContentPane().setBackground(Color.YELLOW);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.YELLOW));
+		});
+		rose.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.PINK);
+			menu.setBackground(Color.PINK);
+			question.setBackground(Color.PINK);reponse.setBackground(Color.PINK);validation.setBackground(Color.PINK);
+			frame.setBackground(Color.PINK);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.PINK));
+			accueil.getContentPane().setBackground(Color.PINK);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.PINK));
+		});
+		orange.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.ORANGE);
+			menu.setBackground(Color.ORANGE);
+			question.setBackground(Color.ORANGE);reponse.setBackground(Color.ORANGE);validation.setBackground(Color.ORANGE);
+			frame.setBackground(Color.ORANGE);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.ORANGE));
+			accueil.getContentPane().setBackground(Color.ORANGE);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.ORANGE));
+		});
+		vert.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.GREEN);
+			menu.setBackground(Color.GREEN);
+			question.setBackground(Color.GREEN);reponse.setBackground(Color.GREEN);validation.setBackground(Color.GREEN);
+			frame.setBackground(Color.GREEN);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.GREEN));
+			accueil.getContentPane().setBackground(Color.GREEN);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.GREEN));
+		});
+		bleu.addActionListener(e ->{
+			for (int i=0;i<4;i++) panelsPersos[i].setBackground(Color.CYAN);
+			menu.setBackground(Color.CYAN);
+			question.setBackground(Color.CYAN);reponse.setBackground(Color.CYAN);validation.setBackground(Color.CYAN);
+			frame.setBackground(Color.CYAN);
+			frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.CYAN));
+			accueil.getContentPane().setBackground(Color.CYAN);
+			accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.CYAN));
+		});
+
+
+		
+		
+		
+		//pour charger une partie ou lancer une nouvelle(pas encore fonctionnel)
+		//new game
+		newf.addActionListener(e ->{
+
+			;});
+
+		//charger une partie
+		load.addActionListener(e->{
+
+			;});
+
+
+
+
+		
 		frame.add(question);
 		frame.add(reponse);
 		frame.add(validation);
@@ -165,6 +261,35 @@ ImageIcon croix= new ImageIcon(new ImageIcon(Jeu.getImage("croix.jpg")).getImage
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.lightGray);
-		frame.setVisible(true);
+		frame.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.WHITE));
+		frame.setVisible(false);
+
+
+
+		//écran d'accueil
+		JButton jouer= new JButton("Cliquer pour jouer");
+		accueil.add(jouer);
+		
+	//rendre le bouton transparent
+    	jouer.setOpaque(false);
+    //enlever la zone de contenu
+    	jouer.setContentAreaFilled(false);
+    //enlever la bordure
+   	 	jouer.setBorderPainted(false);
+   	//pour lancer le jeu
+		jouer.addActionListener(new ActionListener() {     
+
+			public void actionPerformed(ActionEvent e) {
+				accueil.setVisible(false);
+				frame.setVisible(true);				
+			}});
+
+		accueil.pack();
+		accueil.setSize(500, 500);
+		accueil.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		accueil.getContentPane().setBackground(Color.lightGray);
+		accueil.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.WHITE));
+		accueil.setVisible(true);
+
 	}
 }
