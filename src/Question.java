b0275a5ab34ffb1dd63a98c8c2f17ecb5caa6d81
pauloud <paulfontaine2010@hotmail.fr>
2022-03-28 15:14:07 +0200

    ajout code pour export personnages crees (limité)

--- src/Question.java
+++ src/Question.java
@@ -3,6 +3,7 @@ import java.util.function.*;
 class Question {
   
     private Function <Personnage, Boolean> condition = perso -> true ; 
+  
     private static <T,U,V,W> Function<T,W> bind (Function<T,U> ftu,Function<T,V> ftv, BiFunction<U,V,W> fuvw){
       return (t -> fuvw.apply(ftu.apply(t),ftv.apply(t)));
 
@@ -10,7 +11,7 @@ class Question {
     
 
     private void and (Function <Personnage,Boolean> lambdaCond){
-        condition = bind(condition,lambdaCond,((b1,b2) -> b1 & b2));
+        condition = bind(condition,lambdaCond,((b1,b2) -> b1 && b2));
 
     }
     private void or (Function <Personnage,Boolean> lambdaCond){
