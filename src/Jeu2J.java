e3b72197d7e8f9a164649a616f8d8ae15cd4e96a
pauloud <57322619+pauloud@users.noreply.github.com>
2022-04-21 16:46:39 +0200

    Delete Jeu2J.java

--- src/Jeu2J.java
+++
@@ -1,9 +0,0 @@
-public class Jeu2J extends Jeu {
-    private Integer indicePersoChoisi2; 
-    @Override 
-    public String fichierSauvegarde() {
-        return "sauvegarde2.json"; 
-    }
-    
-    
-}
